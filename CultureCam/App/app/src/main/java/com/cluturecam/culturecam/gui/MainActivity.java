207eb9fecd66344b17a62e568cbfd7d82b8da7cd
Bianca Barnucz <bianca.barnucz@gmail.com>
2017-10-20 19:44:00 +0200

    Button

--- CultureCam/App/app/src/main/java/com/cluturecam/culturecam/gui/MainActivity.java
+++ CultureCam/App/app/src/main/java/com/cluturecam/culturecam/gui/MainActivity.java
@@ -2,14 +2,22 @@ package com.cluturecam.culturecam.gui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.cluturecam.culturecam.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b_takepic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        System.out.println("Button funktioniert!!");
     }
 }
