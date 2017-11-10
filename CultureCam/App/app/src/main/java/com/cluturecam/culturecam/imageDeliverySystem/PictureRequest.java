f029cd6925f4a2067633391da55c54320d3586bc
Bianca Barnucz <bianca.barnucz@gmail.com>
2017-11-10 19:05:24 +0100

    Logs

--- CultureCam/App/app/src/main/java/com/cluturecam/culturecam/imageDeliverySystem/PictureRequest.java
+++ CultureCam/App/app/src/main/java/com/cluturecam/culturecam/imageDeliverySystem/PictureRequest.java
@@ -1,9 +1,10 @@
 package com.cluturecam.culturecam.imageDeliverySystem;
 
 
+import android.content.Context;
 import android.graphics.Bitmap;
 
 public interface PictureRequest {
-    Bitmap requestPicture ();
+    Bitmap requestPicture (Context context);
 
 }
