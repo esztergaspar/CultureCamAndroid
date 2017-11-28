5a1571e8928a22107c7708cdf7cf64ce0a51d848
Marlene Mairunteregg <a1309990@unet.univie.ac.at>
2017-11-28 20:33:10 +0100

    test content intent

--- CultureCam/App/app/src/main/java/com/culturecam/culturecam/imageDeliverySystem/CameraService.java
+++ CultureCam/App/app/src/main/java/com/culturecam/culturecam/imageDeliverySystem/CameraService.java
@@ -2,12 +2,16 @@ package com.culturecam.culturecam.imageDeliverySystem;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 
 public class CameraService implements PictureRequest{
     @Override
     public Bitmap requestPicture(Context context) {
-
+        //TODO: delete this, just for testing olé
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        //activity.startActivityForResult(intent, 1001);
         return null;
     }
 }
