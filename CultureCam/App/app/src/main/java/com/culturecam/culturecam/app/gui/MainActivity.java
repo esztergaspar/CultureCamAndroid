1871e93a95282550591b320025bd80eedfc0e0c1
esztergaspar <esztergaspar7@gmail.com>
2017-12-01 13:27:30 +0100

    add user feature camera

--- CultureCam/App/app/src/main/java/com/culturecam/culturecam/app/gui/MainActivity.java
+++ CultureCam/App/app/src/main/java/com/culturecam/culturecam/app/gui/MainActivity.java
@@ -11,6 +11,8 @@ import com.culturecam.culturecam.R;
 import com.culturecam.culturecam.app.system.ImageSearchService;
 import com.culturecam.culturecam.entities.SearchEngines;
 
+import static com.culturecam.culturecam.app.gui.activity_chooseimage.REQUEST_IMAGE_CAPTURE;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
