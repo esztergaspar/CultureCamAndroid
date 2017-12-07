c0acff9435891a0d953cff86ad229265ea7ec7ea
Marlene Mairunteregg <a1309990@unet.univie.ac.at>
2017-12-07 21:42:36 +0100

    save photo on internal datasystem

--- CultureCam/App/app/src/main/java/com/culturecam/culturecam/app/gui/activity_chooseimage.java
+++ CultureCam/App/app/src/main/java/com/culturecam/culturecam/app/gui/activity_chooseimage.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,11 @@ import com.culturecam.culturecam.app.system.ImageSearchService;
 
 import android.util.Log;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /*
 * https://developer.android.com/training/camera/photobasics.html#TaskCaptureIntents*/
 
@@ -23,8 +30,11 @@ public class activity_chooseimage extends AppCompatActivity {
     private static final String TAG = "activity_chooseimage";
     private ImageDeliveryController imageDeliveryController;
     private ImageSearchService imageSearchService;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int SELECT_PICTURE = 2;
+    private String mCurrentPhotoPath;
+    private Uri photoURI;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int SELECT_PICTURE = 2;
+    public static final String IMAGE_URI = "image_uri";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,11 +67,26 @@ public class activity_chooseimage extends AppCompatActivity {
         }
 
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            //finish();
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e(TAG,"Error creating file");
+                return;
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.cluturecam.culturecam.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
-
     }
 
     @Override
@@ -69,26 +94,28 @@ public class activity_chooseimage extends AppCompatActivity {
         Log.d(TAG, "in onActivityResult method");
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-
-            Log.d(TAG, "received image from camera");
+            Log.d(TAG, "received image");
 
             Intent intent = new Intent(this, LoadViewActivity.class);
-            intent.putExtras(data.getExtras());
+            intent.putExtra(IMAGE_URI,mCurrentPhotoPath);
             startActivity(intent);
         }
+    }
 
-        if (requestCode == SELECT_PICTURE && resultCode == RESULT_OK) {
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getApplication().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
-            Uri imageUri = data.getData();
-            //TODO
-            //Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
 
-            Log.d(TAG, "received image from media library");
-
-            Intent intent = new Intent(this, LoadViewActivity.class);
-            intent.putExtras(data.getExtras());
-            startActivity(intent);
-        }
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
